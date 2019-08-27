f88c72c66f897f979a2aba49100456ec04f1336b
Alexis Haumaitre <haumalex@gmail.com>
2019-08-27 16:27:04 +0200

    Update controllers/admin/AdminPsThemeCustoConfiguration.php
    Co-Authored-By: Thomas N <Quetzacoalt91@users.noreply.github.com>

--- controllers/admin/AdminPsThemeCustoConfiguration.php
+++ controllers/admin/AdminPsThemeCustoConfiguration.php
@@ -23,7 +23,7 @@
 * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
 * International Registered Trademark & Property of PrestaShop SA
 **/
-require _PS_MODULE_DIR_ . 'ps_themecusto/vendor/autoload.php';
+require_once _PS_MODULE_DIR_ . 'ps_themecusto/vendor/autoload.php';
 
 class AdminPsThemeCustoConfigurationController extends ModuleAdminController
 {
