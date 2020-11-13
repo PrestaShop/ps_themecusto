3cb1ebd290b54e4918e6a7d58437afe2187a3597
Progi1984 <franck.lefevre@prestashop.com>
2020-11-13 14:08:07 +0100

    Fixed CI PHP

--- translations/fr.php
+++ translations/fr.php
@@ -1,7 +1,7 @@
 <?php
 
 global $_MODULE;
-$_MODULE = array();
+$_MODULE = [];
 $_MODULE['<{ps_themecusto}prestashop>ps_themecusto_95db0d5190e9cdbed521ecf6ea2373ca'] = 'Personnalisation de thème';
 $_MODULE['<{ps_themecusto}prestashop>ps_themecusto_c3db892b3555ae0538402754d0c375d0'] = 'Configurez et personnalisez facilement le thème de votre page d\'accueil et vos principaux modules natifs. Fonctionnalité disponible dans la page Apparence > Theme & Logo.';
 $_MODULE['<{ps_themecusto}prestashop>ps_themecusto_f87b9438e8de65a83e7a3ef56a9d23fb'] = 'Une erreur est survenue pendant l\'installation. Veuillez nous contacter via le site PrestaShop Addons';
